faa28123e156ffe753c72ad3972d0c9c62a0868e
Guangchu Shi <rollschild@gmail.com>
2018-02-15 13:12:08 -0500

    Update MaximizeRatings.cpp

--- MaximizeRatings.cpp
+++ MaximizeRatings.cpp
@@ -5,34 +5,33 @@
 using namespace std;
 
 int maxRatings(vector<int> ratings) {
-    // int result = 0;
     vector<int> results;
     results.push_back(0);
-    // cout << results.size() << endl;
-    // cout << ratings.size() << endl;
-    // vector<int>::iterator itr;
     for(int i = 0; i < ratings.size()-1; ++i) {
         int tempsize = results.size();
-        // cout << tempsize << endl;
-        // int sum1 = ratings[i+1];
-        // int sum2 = ratings[i+2];
+        int howmany = 0;
         for(int j = 0; j < tempsize; ++j) {
-            if(i+j < ratings.size())
+            if(i+j < ratings.size()) {
                 results.push_back(results[j] + ratings[i+j]);
-            if(i+j+1 < ratings.size())
+                ++howmany;
+            }
+
+            else {
+                results.push_back(results[j]);
+            }
+
+            if(i+j+1 < ratings.size()) {
                 results.push_back(results[j] + ratings[i+1+j]);
+                ++howmany;
+            }
         }
 
-        /*
-        for(auto c : results) {
-            cout << c << " ";
-        }
-        cout << endl;
-        */
-        if(i == ratings.size() - 2) continue;
-        results.erase(results.begin(), results.begin()+tempsize);
+        if(i == ratings.size() - 2) {
+            results.erase(results.begin(), results.begin()+tempsize-1);
+        }                                                                       
+        else
+            results.erase(results.begin(), results.begin()+tempsize);
     }
-    // sort(results.begin(), results.end());
     for(auto c : results) {
         cout << c << " ";
     }
@@ -41,9 +40,10 @@ int maxRatings(vector<int> ratings) {
 }
 
 int main() {
-    vector<int> vec = {-1, -2, -3, -4, -5};
+    vector<int> vec1 = {-1, -2, -3, -4, -5};
     vector<int> vec2 = {9, -1, -3, 4, 5};
-    int result = maxRatings(vec2);                                              
+    vector<int> vec3 = {-1, -2, -3};
+    int result = maxRatings(vec1);
     cout << result << endl;
     return 0;
 }
