f14ef5bb2cc5bcdc85636a460aa72588812d28c7
rollschild <rollschild@gmail.com>
2018-07-09 11:26:09 -0400

    UniquePaths unoptimized version

---
+++ UniquePaths.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+// Unoptimized version
+using namespace std;
+
+class Solution {
+public:
+ int uniquePaths(int m, int n) {
+   vector<vector<int> > paths(m, vector<int>(n, 1));
+   for (int i = 1; i < m; ++i) {
+     for (int j = 1; j < n; ++j) {
+       paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
+     }
+   }
+   return paths[m - 1][n - 1];
+    }
+};
+
+int main() {
+  Solution solution;
+  int result = solution.uniquePaths(3, 2);
+  cout << result << endl;
+
+  return 0;
+}
