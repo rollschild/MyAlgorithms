59109e059da23a24a08136dfc78c71c14503ffcc
Guangchu Shi <rollschild@gmail.com>
2018-09-18 17:35:39 -0400

    Sum all primes loe than a num

---
+++ sum_all_primes.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int sum(int num) {
+  vector<bool> vec(num+1);
+  fill_n(vec.begin(), 2, false);
+  fill_n(vec.begin() + 2, num - 1, true);
+  int ele = 2;
+  while (ele <= num) {
+    if (!vec[ele]) {
+      ++ele;
+      continue;
+    }
+    for (int i = 0; i <= num; ++i) {
+      int product = ele * (ele + i);
+      if (product <= num)
+        vec[product] = false;
+      else break;
+    }
+    ++ele;
+  }
+  int sum = 0;
+  for (int j = 2; j <= num; ++j) {
+    if (vec[j]) sum += j;
+  }
+  return sum;
+}
+
+int main() {
+  cout << sum(977) << endl;
+  return 0;
+}
